0e9206360d1585f903f3b8f6ec295e1f1f16b219
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-03-31 13:38:03 +0200

    La till Identity

---
+++ ASP.NET Projekt/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ASP.NET_Projekt.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASP.NET_Projekt.Areas.Identity.IdentityHostingStartup))]
+namespace ASP.NET_Projekt.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ASPNET_ProjektContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ASPNET_ProjektContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ASPNET_ProjektContext>();
+            });
+        }
+    }
+}
