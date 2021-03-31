1804e65cd594336f61537d0ab20cc242d9d1d1f3
samuelbrinke <samuel.brinkeback@hotmail.com>
2021-03-31 14:58:06 +0200

    Lagt till Modeller

---
+++ ASP.NET Projekt/Model/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Projekt.Model
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+        public int SpotsAvailable { get; set; }
+
+        public List<User> Organizer { get; set; }
+        public List<User> Attendees { get; set; }
+    }
+}
