daae7bbae6053a05463b4220a967b9c9a0312a30
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-04-01 09:53:04 +0200

    Leker med registersidan

--- ASP.NET Projekt/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ASP.NET Projekt/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,10 @@ namespace ASP.NET_Projekt.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
