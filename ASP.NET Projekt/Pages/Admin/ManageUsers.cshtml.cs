fb299abb965a7c623161e713f9b75256313b454b
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-04-06 14:49:35 +0200

    La till manageUsers sida för admins

---
+++ ASP.NET Projekt/Pages/Admin/ManageUsers.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP.NET_Projekt.Data;
+using ASP.NET_Projekt.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace ASP.NET_Projekt.Pages.Admin
+{
+    [Authorize(Roles = "Administrator")]
+    public class ManageUsersModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;
+
+        public ManageUsersModel(ApplicationDbContext context, RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IList<User> Users { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Users = await _context.Users.ToListAsync();
+        }
+    }
+}
