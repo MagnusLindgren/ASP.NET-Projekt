6e1c8365ccf3b91b9249505e468367f9585a0ed1
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-04-07 10:04:27 +0200

    La till UserDetails för Admins samt lite kod städning

--- ASP.NET Projekt/Model/User.cs
+++ ASP.NET Projekt/Model/User.cs
@@ -8,7 +8,6 @@ namespace ASP.NET_Projekt.Model
 {
     public class User : IdentityUser
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
